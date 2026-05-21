0a4d0b29e2ae20d94a66a73867908ac6cb74a043
jrworthamhorn <jrwortham_horn@yahoo.com>
2026-05-21 12:33:20 -0500

    Create htsData.js

---
+++ src/data/htsData.js
@@ -0,0 +1,10 @@
+export const htsData = [
+  {
+    code: "8544429090",
+    description: "Insulated electric conductors"
+  },
+  {
+    code: "85011040",
+    description: "Electric motors"
+  }
+];
