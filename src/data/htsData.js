3509165e2880f02d95cf96af5b9b5b5a97ef9d4e
jrworthamhorn <jrwortham_horn@yahoo.com>
2026-05-21 17:34:05 -0500

    Update htsData.js

--- src/data/htsData.js
+++ src/data/htsData.js
@@ -1,10 +1,48 @@
+import { part1 } from "./hts/part1";
+import { part2 } from "./hts/part2";
+import { part3 } from "./hts/part3";
+import { part4 } from "./hts/part4";
+import { part5 } from "./hts/part5";
+import { part6 } from "./hts/part6";
+import { part7 } from "./hts/part7";
+import { part8 } from "./hts/part8";
+import { part9 } from "./hts/part9";
+import { part10 } from "./hts/part10";
+import { part11 } from "./hts/part11";
+import { part12 } from "./hts/part12";
+import { part13 } from "./hts/part13";
+import { part14 } from "./hts/part14";
+import { part15 } from "./hts/part15";
+import { part16 } from "./hts/part16";
+import { part17 } from "./hts/part17";
+import { part18 } from "./hts/part18";
+import { part19 } from "./hts/part19";
+import { part20 } from "./hts/part20";
+import { part21 } from "./hts/part21";
+import { part22 } from "./hts/part22";
+import { part23 } from "./hts/part23";
+import { part24 } from "./hts/part24";
+import { part25 } from "./hts/part25";
+import { part26 } from "./hts/part26";
+import { part27 } from "./hts/part27";
+import { part28 } from "./hts/part28";
+import { part29 } from "./hts/part29";
+import { part30 } from "./hts/part30";
+import { part31 } from "./hts/part31";
+import { part32 } from "./hts/part32";
+import { part33 } from "./hts/part33";
+import { part34 } from "./hts/part34";
+import { part35 } from "./hts/part35";
+import { part36 } from "./hts/part36";
+import { part37 } from "./hts/part37";
+
 export const htsData = [
-  {
-    code: "8544429090",
-    description: "Insulated electric conductors"
-  },
-  {
-    code: "85011040",
-    description: "Electric motors"
-  }
+  ...part1, ...part2, ...part3, ...part4, ...part5,
+  ...part6, ...part7, ...part8, ...part9, ...part10,
+  ...part11, ...part12, ...part13, ...part14, ...part15,
+  ...part16, ...part17, ...part18, ...part19, ...part20,
+  ...part21, ...part22, ...part23, ...part24, ...part25,
+  ...part26, ...part27, ...part28, ...part29, ...part30,
+  ...part31, ...part32, ...part33, ...part34, ...part35,
+  ...part36, ...part37
 ];
